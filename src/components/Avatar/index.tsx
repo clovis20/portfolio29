0b369d4e12d402ca75c9da542290717e6a7cbfac
Clovis <clovis20@gmail.com>
2023-05-30 15:37:04 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="http://github.com/clovis20.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/clovis20.png" />
-    <Titulo fontSize={20}>Clovis Junior</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Clovis Junior</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        clovis20
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
